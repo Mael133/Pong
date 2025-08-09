670ef02b5bfc69afa2f2f105cc061ee78455f792
Ryan-0-26 <ryanvicto74@gmail.com>
2025-08-09 19:23:37 -0300

    Correções no sistema de colisões e implementação.
    Quando a bola batia na lateral da raquete,
    ela entrava na raquetekkkkkk
    
    Agora é pra estar corrigido, e a bola não entra mais na raquete.
    E tiveram algumas em como a função que checa
    colisões funciona. Como a gente já sabe o raio da
    bola com antecedência, não é necessário passar
    como parâmetro.

--- main.py
+++ main.py
@@ -7,18 +7,19 @@ pygame.display.set_caption("Pong")
 CLOCK = pygame.time.Clock()
 
 bola = circulo(LARGURA//2, ALTURA//2, 7)
-raqueteJogador = retangulo(LARGURA-60, (ALTURA-200)/2, 40, 200)
-raqueteOponente = retangulo(20, (ALTURA-200)/2, 40, 200)
+raqueteOponente = retangulo(LARGURA-RAQUETE_LARGURA-20, (ALTURA-RAQUETE_ALTURA)/2, 
+                           RAQUETE_LARGURA, RAQUETE_ALTURA)
+raqueteJogador = retangulo(20, (ALTURA-RAQUETE_ALTURA)/2, 
+                            RAQUETE_LARGURA, RAQUETE_ALTURA)
 
+XVALUE = 0
+YVALUE = 1
 
-jogo = {
-    "ballVelX":6,
-    "ballVelY":6,
-    "ballX": LARGURA//2,
-    "ballY": ALTURA//2,
-    "oponentY": ALTURA//2,
-    "selfY": ALTURA//2
-}
+velocidadeBolaYMinima = 3
+velocidadeBolaYMaxima = 12
+
+score = 0
+oponenteScore = 0
 
 rodando = True
 while rodando:
@@ -29,26 +30,61 @@ while rodando:
     teclas = pygame.key.get_pressed()
 
     # --- Logica ---
-    if teclas[pygame.K_UP]: 
-        bola.y -= VELOCIDADE_INICIAL_BOLA
-    if teclas[pygame.K_DOWN]:
-        bola.y += VELOCIDADE_INICIAL_BOLA
-    if teclas[pygame.K_LEFT]:
-        bola.x -= VELOCIDADE_INICIAL_BOLA
-    if teclas[pygame.K_RIGHT]:
-        bola.x += VELOCIDADE_INICIAL_BOLA
+
+    # mover raquete
+    movendo = 0
+    if teclas[pygame.K_UP] and raqueteJogador.y > 0: 
+        raqueteJogador.y -= VELOCIDADE_RAQUETE
+        movendo = -1
+    if teclas[pygame.K_DOWN] and ((raqueteJogador.y+raqueteJogador.altura) < ALTURA):
+        raqueteJogador.y += VELOCIDADE_RAQUETE
+        movendo = 1
+
+    bola.x += velocidadeBola[XVALUE]
+    bola.y += velocidadeBola[YVALUE]
+
+    # colisão
     
-    cor = BRANCO
-    if colisao(bola, raqueteJogador):
-        cor = (255,0,0)
-    
+    if bola.x < 55 or bola.x > LARGURA-55:
+        # o número mágico (55) vem do fato de que a bola tem que estar pelo menos
+        # a uma distância de
+        # 20 (raquete até parede) + 20 (largura da raquete) + 14 (diametro da bola)
+        # isso dá 54, aí eu adicionei 1 por desencargo de consciência.
+        # Vai que o código quebra, sla.
+        if colisao(bola.x, bola.y, raqueteJogador
+        ) or colisao(bola.x, bola.y, raqueteOponente): 
+            bola.x -= velocidadeBola[XVALUE]
+            bola.y -= velocidadeBola[YVALUE]
+            if bola.x < (20+raqueteOponente.largura) or bola.x > (LARGURA-20-raqueteOponente.largura):
+                velocidadeBola[YVALUE] *= -1
+            else:
+                velocidadeBola[XVALUE] *= -1
+
+    if bola.y <= 0 or bola.y >= ALTURA: 
+        velocidadeBola[YVALUE] *= -1
+
+    if bola.x+velocidadeBola[XVALUE] <= 0:
+        velocidadeBola = (VELOCIDADE_INICIAL_BOLA*-1).copy()
+        bola.x = LARGURA/2
+        bola.y = ALTURA/2
+        velocidadeBola[XVALUE] *= -1
+    elif bola.x+velocidadeBola[XVALUE] >= LARGURA:
+        velocidadeBola = VELOCIDADE_INICIAL_BOLA.copy()
+        bola.x = LARGURA/2
+        bola.y = ALTURA/2
+        velocidadeBola[XVALUE] *= -1
+
+
+    # mover bola
+
 
     # --- Construção da tela ---
     tela.fill(COR_FUNDO)
-    pygame.draw.circle(tela, cor, (bola.x, bola.y), bola.raio)
-    pygame.draw.rect(tela, cor, raqueteJogador.rect)
-    #pygame.draw.rect(tela, BRANCO, raqueteOponente.rect)
-
+    pygame.draw.circle(tela, COR_OBJETOS, (bola.x, bola.y), bola.raio)
+    pygame.draw.rect(tela, COR_OBJETOS, 
+    (raqueteJogador.x, raqueteJogador.y, raqueteJogador.largura, raqueteJogador.altura))
+    pygame.draw.rect(tela, COR_OBJETOS, 
+    (raqueteOponente.x, raqueteOponente.y, raqueteOponente.largura, raqueteOponente.altura))
 
     # --- Atualiza a Tela ---
     pygame.display.flip()
--- pong.py
+++ pong.py
@@ -3,11 +3,13 @@ import numpy as np
 
 LARGURA, ALTURA = 800, 600
 COR_FUNDO = pygame.Color('grey12')
+COR_OBJETOS = pygame.Color('white')
 BRANCO = (255, 255, 255)
 RAQUETE_LARGURA, RAQUETE_ALTURA = 20, 100
 BOLA_RAIO = 7
 VELOCIDADE_RAQUETE = 6
-VELOCIDADE_INICIAL_BOLA = 7
+VELOCIDADE_INICIAL_BOLA = np.array([7,7])
+velocidadeBola = np.array([7,7])
 
 class circulo:
     def __init__(self, x, y, raio):
@@ -21,27 +23,22 @@ class retangulo:
         self.y = y
         self.largura = largura
         self.altura = altura
-        self.rect = (x, y, largura, altura)
 
-def colisao(circulo, ret):
-
-    print(f"DEBUG: Tipo de circulo.x = {type(circulo.x)}, Valor = {circulo.x}")
-    print(f"DEBUG: Tipo de ret.x = {type(ret.x)}, Valor = {ret.x}")
-
+def colisao(circulox, circuloy, ret, raio=BOLA_RAIO):
     centroRet = [0,0]
     distancia = [0,0]
     centroRet[0] = ret.x + ret.largura/2
     centroRet[1] = ret.y + ret.altura/2
-    distancia[0] = abs(circulo.x - centroRet[0])
-    distancia[1] = abs(circulo.y - centroRet[1])
+    distancia[0] = abs(circulox - centroRet[0])
+    distancia[1] = abs(circuloy - centroRet[1])
 
-    if distancia[0] > (ret.largura/2 + circulo.raio): return False
-    if distancia[1] > (ret.altura/2 + circulo.raio): return False
+    if distancia[0] > (ret.largura/2 + raio): return False
+    if distancia[1] > (ret.altura/2 + raio): return False
 
     if (distancia[0] <= (ret.largura/2)): return True 
     if (distancia[1] <= (ret.altura/2)): return True
 
     distanciaCantoQ = (distancia[0] - ret.largura/2)**2 + (distancia[1] - ret.altura/2)**2
 
-    return (distanciaCantoQ <= (circulo.raio**2))
+    return (distanciaCantoQ <= (raio**2))
 
